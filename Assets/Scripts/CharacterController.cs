1c40b66097536b11311259d88f5af6fa723c14d4
llanglois64 <leo.langlois@mail.mcgill.ca>
2025-01-11 01:44:09 -0500

    Player controller stuff

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -15,6 +15,7 @@ public class CharacterController : MonoBehaviour
     [SerializeField] private LayerMask ground;
     private float speedFactor;
     private bool isGrounded;
+    private float extraJumpTime;
 
     void Start()
     {
@@ -25,37 +26,31 @@ public class CharacterController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        //float deltaX = Input.GetAxis("Horizontal") * speed;
-        //Vector2 movement = new Vector2(deltaX, _rb.linearVelocity.y);
         float inputX = Input.GetAxis("Horizontal");
         _rb.linearVelocity += new Vector2(inputX * speed * Time.fixedDeltaTime, 0) * speedFactor;
 
-        /*Vector3 max = _box.bounds.max;
-        Vector3 min = _box.bounds.min;
-        Vector2 corner1 = new Vector2(max.x, min.y - 0.1f);
-        Vector2 corner2 = new Vector2(min.x, min.y - 0.2f);
-        Collider2D hit = Physics2D.OverlapArea(corner1, corner2);
-        bool isGrounded = hit != null && hit.gameObject.layer == LayerMask.NameToLayer("Ground");
-        */
         isGrounded = Physics2D.CircleCast(checkPosition.position, 0.1f, Vector2.zero, 0, ground);
 
-        // This is the case for slope, gravity will pull it down
-        
-        //_rb.gravityScale = (isGrounded && Mathf.Approximately(deltaX, 0)) ? 0 : 1;
+        moveSettings(inputX);
+    }
 
-
+    void moveSettings(float inputX){
         if(isGrounded == true){
+            //on ground
+            //move faster, grippier
             _rb.linearDamping = 5f;
             speedFactor = 1;
         }
         else{
             //in air
+            //move slower, less drag
             _rb.linearDamping = 4;
             speedFactor = 0.7f;
         }
 
+        //slow rise, fast fall
         if(_rb.linearVelocityY > 2){
-            _rb.gravityScale = 0.5f;
+            _rb.gravityScale = 1f;
         }
         else if(isGrounded == true){
             _rb.gravityScale = 0;
@@ -64,20 +59,32 @@ public class CharacterController : MonoBehaviour
             _rb.gravityScale = 2f;
         }
 
+        //direction of the player
         float direction = _rb.linearVelocityX / Mathf.Abs(_rb.linearVelocityX);
 
+        //faster turning
         if(direction != inputX){
             speedFactor = 2f;
         }
 
+        //extra x drag
         _rb.linearVelocity *= new Vector2(1 - Time.fixedDeltaTime * 2, 1);
+
+        //cayote time
+        if(isGrounded == true){
+            extraJumpTime = 0.1f;
+        }
+        extraJumpTime -= Time.deltaTime;
     }
 
     void Update(){
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            //_rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            _rb.linearVelocityY = jumpForce;
+        //single time inputs are in Update function
+        //jumping
+        if(extraJumpTime > 0){
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                _rb.linearVelocityY = jumpForce;
+            }
         }
     }
 
