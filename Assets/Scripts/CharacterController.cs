a9e081d07812efaa494fc23072129bc7711380ae
Orisu <66352948+Orisu179@users.noreply.github.com>
2025-01-10 20:42:57 -0500

    More git ignore changes

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -5,8 +5,7 @@ public class CharacterController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Hello game jam");
-        
+
     }
 
     // Update is called once per frame
