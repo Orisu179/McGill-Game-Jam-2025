3c029d8bf8e04e673b5d97593b4c9a4eae2a4e1a
Orisu <66352948+Orisu179@users.noreply.github.com>
2025-01-10 19:38:38 -0500

    Initial Commit
    First commit

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hello game jam");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
