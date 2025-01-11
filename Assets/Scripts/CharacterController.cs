8d12d466f18a2c27e2410c5c1865114d5c82cfd3
llanglois64 <leo.langlois@mail.mcgill.ca>
2025-01-11 00:00:42 -0500

    New Character

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -9,7 +9,8 @@ public class CharacterController : MonoBehaviour
     [SerializeField] private float delayBetweenDashPress = 0.25f;
     private BoxCollider2D _box;
     private Rigidbody2D _rb;
-    [SerializeField] private GameObject checkPosition;
+    [SerializeField] private Transform checkPosition;
+    [SerializeField] private LayerMask ground;
 
     void Start()
     {
@@ -24,20 +25,23 @@ public class CharacterController : MonoBehaviour
         Vector2 movement = new Vector2(deltaX, _rb.linearVelocity.y);
         _rb.linearVelocity = movement;
 
-        Vector3 max = _box.bounds.max;
+        /*Vector3 max = _box.bounds.max;
         Vector3 min = _box.bounds.min;
         Vector2 corner1 = new Vector2(max.x, min.y - 0.1f);
         Vector2 corner2 = new Vector2(min.x, min.y - 0.2f);
         Collider2D hit = Physics2D.OverlapArea(corner1, corner2);
         bool isGrounded = hit != null && hit.gameObject.layer == LayerMask.NameToLayer("Ground");
-
-        //bool isGrounded = false;
+        */
+        bool isGrounded = Physics2D.CircleCast(checkPosition.position, 1, Vector2.zero, 0, ground);
 
         // This is the case for slope, gravity will pull it down
-        _rb.gravityScale = (isGrounded && Mathf.Approximately(deltaX, 0)) ? 0 : 1;
+        
+        //_rb.gravityScale = (isGrounded && Mathf.Approximately(deltaX, 0)) ? 0 : 1;
+        
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            _rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            //_rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            _rb.linearVelocityY = jumpForce;
         }
 
     }
