d4ea3541e302fbf1e4c7d5814db939c2d6e4cab7
Orisu <66352948+Orisu179@users.noreply.github.com>
2025-01-10 23:20:39 -0500

    Controller is working

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -1,16 +1,47 @@
+using System;
 using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private float speed = 4.5f;
+    [SerializeField] private float jumpForce = 12f;
+    [SerializeField] private float delayBetweenDashPress = 0.25f;
+    private BoxCollider2D _box;
+    private Rigidbody2D _rb;
+
     void Start()
     {
-
+       _rb = GetComponent<Rigidbody2D>();
+       _box = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        Vector2 movement = new Vector2(deltaX, _rb.linearVelocity.y);
+        _rb.linearVelocity = movement;
+
+        Vector3 max = _box.bounds.max;
+        Vector3 min = _box.bounds.min;
+        Vector2 corner1 = new Vector2(max.x, min.y - 0.1f);
+        Vector2 corner2 = new Vector2(min.x, min.y - 0.2f);
+        Collider2D hit = Physics2D.OverlapArea(corner1, corner2);
+        bool isGrounded = hit != null;
+
+        // This is the case for slope, gravity will pull it down
+        _rb.gravityScale = (isGrounded && Mathf.Approximately(deltaX, 0)) ? 0 : 1;
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            _rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+
+    }
+
+    //TODO: do the dash
+    private void Dash()
+    {
+
     }
 }
