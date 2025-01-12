ea7d8da8dd970ed5275e227a211759075dd07b9f
walidmenou <walid.menou@outlook.com>
2025-01-12 02:32:04 -0500

    portal

--- Assets/Scripts/MechanicInteraction.cs
+++ Assets/Scripts/MechanicInteraction.cs
@@ -22,41 +22,10 @@ public class MechanicInteraction : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        if(inThought){
-            transform.Rotate(0,0,ThoughtRotationSpeed*Time.deltaTime);
-            
-            if(Input.GetKeyDown(KeyCode.Space)){
-                _rb.linearVelocity = mySprite.transform.up * ThoughtShotSpeed;
-                _rb.gravityScale = 1;
-                inThought = false;
 
-				StartCoroutine(wait());
-                
-            }
-        }
-        else
-        {
-            lerpRotate(mySprite, 0, 5);
-        }
     }
-	
-	private IEnumerator wait()
-	{
-    	yield return new WaitForSeconds(0.06f);
-    	_bc.enabled = true;   // Re-enable player collider
-	}
-	
-    private void lerpRotate(Transform setter, float angle, float speed)
-    {
-        Vector3 originalAngle = setter.eulerAngles;
-        setter.eulerAngles = new Vector3(0, 0, angle);
-        Quaternion to = setter.rotation;
-
-        setter.eulerAngles = originalAngle;
-        setter.rotation = Quaternion.SlerpUnclamped(setter.rotation, to, Time.deltaTime * speed);
-    }
-
+    
+    
     private void OnCollisionEnter2D(Collision2D other) {
         if(other.gameObject.tag == "Spring"){
             Instantiate(bangEffect, transform.position, Quaternion.identity);
@@ -68,16 +37,13 @@ public class MechanicInteraction : MonoBehaviour
             _rb.linearVelocity = new Vector2(_rb.linearVelocityX, 12);
             transform.position = other.transform.position;
         }
-        else if (other.gameObject.tag == "Thought")
-        {
-            inThought = true;
-            transform.position = other.gameObject.transform.position;
-            _rb.gravityScale = 0;
-			_bc.enabled = false;
-        }
         else if (other.gameObject.tag == "Border")
         {
             transform.position = _startPos;
+        }
+        else if (other.gameObject.tag == "Portal")
+        {
+            
         }
     }
 }
