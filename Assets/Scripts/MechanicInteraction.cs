87f27082f1957a2830c242c27d8034167f136ebf
llanglois64 <leo.langlois@mail.mcgill.ca>
2025-01-12 10:16:29 -0500

    No mo lerp

--- Assets/Scripts/MechanicInteraction.cs
+++ Assets/Scripts/MechanicInteraction.cs
@@ -3,13 +3,9 @@ using System.Collections;
 public class MechanicInteraction : MonoBehaviour
 {
     private Rigidbody2D _rb;
-    private BoxCollider2D _bc;
     private Vector3 _startPos;
     private EffectsAudioControl _audioControl;
     public GameObject bangEffect;
-    public static bool inThought;
-    public float ThoughtShotSpeed;
-    public float ThoughtRotationSpeed;
     public Transform mySprite;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -18,45 +14,11 @@ public class MechanicInteraction : MonoBehaviour
         _startPos = transform.position;
         _rb = gameObject.GetComponent<Rigidbody2D>();
         _audioControl = gameObject.GetComponentInChildren<EffectsAudioControl>();
-        _bc = gameObject.GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if(inThought){
-            transform.Rotate(0,0,ThoughtRotationSpeed*Time.deltaTime);
-
-            if(Input.GetKeyDown(KeyCode.Space)){
-                _rb.linearVelocity = mySprite.transform.up * ThoughtShotSpeed;
-                _rb.gravityScale = 1;
-                inThought = false;
-
-				StartCoroutine(wait());
-
-            }
-        }
-        else
-        {
-            lerpRotate(mySprite, 0, 5);
-        }
-    }
-
-	private IEnumerator wait()
-	{
-    	yield return new WaitForSeconds(0.06f);
-    	_bc.enabled = true;   // Re-enable player collider
-	}
-
-    private void lerpRotate(Transform setter, float angle, float speed)
-    {
-        Vector3 originalAngle = setter.eulerAngles;
-        setter.eulerAngles = new Vector3(0, 0, angle);
-        Quaternion to = setter.rotation;
-
-        setter.eulerAngles = originalAngle;
-        setter.rotation = Quaternion.SlerpUnclamped(setter.rotation, to, Time.deltaTime * speed);
     }
 
     private void OnCollisionEnter2D(Collision2D other) {
